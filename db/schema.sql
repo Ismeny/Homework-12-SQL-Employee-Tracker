187368e9b5d10f5283f6c39e184f579488483a06
Ismeny Saguilan <menysag2@gmail.com>
2021-10-25 16:47:25 -0500

    finished scheema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS test_db;
-CREATE DATABASE test_db;
+DROP DATABASE IF EXISTS friends_db;
+CREATE DATABASE friends_db;
 
-USE test_db;
+USE friends_db;
 
 CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL,
@@ -12,15 +12,15 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INT ,
+    salary DECIMAL(10,2) NOT NULL, 
+    department_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL 
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -29,6 +29,5 @@ CREATE TABLE employee (
   PRIMARY KEY (id),
   FOREIGN KEY (role_id)
   REFERENCES roles(id)
-  ON DELETE SET NULL 
-
+  ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,42 @@
 INSERT INTO department (department_name)
-VALUES  ("Authorizations"),
-        ("Appeals");
+VALUES  ("Marketing"),
+        ("Finance"),
+        ("Human Resources"),
+        ("Management"),
+        ("Sales"),
+        ("Engineering"),
+        ("Legal");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES  ("Referral Specialist", 600000, 1),
-        ("Appeals Coordinator", 600000, 2);
+VALUES  ("Social Media Marketer", 60000, 1),
+        ("Banker", 70000, 2),
+        ("HR Compliance Officer", 50000, 3),
+        ("Manager", 100000,4),
+        ("Salesperson", 42000, 5),
+        ("Senior Software Engineer", 500000, 6),
+        ("Lawyer", 140000, 7);
 
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES  ("Ismeny", "Saguilan",1),
-        ("Gina", "Gonzalez", 2),
 
 
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES  ("Rachel", "Green", 6),
+        ("Monica", "Geller", 2),
+        ("Chandler", "Bing", 7),
+        ("Phoebe", "Buffay", 1),
+        ("Ross", "Geller", 4),
+        ("Joey", "Tribbiani", 7),
+        ("Mike", "Hannigan", 3),
+        ("Janice", "Goralnik", 3),
+        ("Richard", "Burke", 5),
+        ("Jill", "Green", 1),
+        ("Charlie", "Wheeler", 2),
+        ("Amy", "Green", 1),
+        ("Jack", "Geller", 2),
+        ("Judy", "Geller", 4),
+        ("Sarah", "Tuttle", 6);
+
+UPDATE employees
+SET manager_id = 13
+WHERE role_id IN (1,3,4,5,6,7)
